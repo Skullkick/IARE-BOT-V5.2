e911c5af59cb101ba100d5fdb65124aea9ab412d
Skullkick <121807883+Skullkick@users.noreply.github.com>
2024-07-04 22:35:32 +0530

    Implemented functions for managing CIE tracker details in the cie_tracker table within managers.db.

--- DATABASE/managers_handler.py
+++ DATABASE/managers_handler.py
@@ -157,3 +157,83 @@ async def get_cgpa_tracker_details(chat_id):
     except Exception as e:
         print(f"Error retrieving the cgpa tracker details : {e}")
         return False
+
+
+async def store_cie_tracker_details(chat_id,status,current_cie_marks):
+    """
+
+    This function is used to store the cie_tracker details
+    :param chat_id: Chat id of the user
+    :param status: Boolean value which is used to stop the tracker
+    :param current_cie_marks: Current cie marks of the user
+    """
+    try:
+        with sqlite3.connect(MANAGERS_DATABASE) as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM cie_tracker WHERE chat_id = ?",(chat_id,))
+            data = cursor.fetchone()
+            if data:
+                cursor.execute("UPDATE cie_tracker SET status = ?,current_cie_marks = ? WHERE chat_id = ?",(status,current_cie_marks,chat_id))
+            else:
+                cursor.execute("INSERT INTO cie_tracker (chat_id,status,current_cie_marks) VALUES (?,?,?)",(chat_id,status,str(current_cie_marks)))
+            conn.commit()
+            return True
+    except Exception as e:
+        print(f"Error storing cie tracker details : {e}")
+        return False
+async def remove_cie_tracker_details(chat_id):
+    """
+    This function is used to remove the row in cie_tracker table based on the chat_id
+    :param chat_id: Chat id of the user
+    """
+    try:
+        with sqlite3.connect(MANAGERS_DATABASE) as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM cie_tracker WHERE chat_id = ?",(chat_id,))
+            data = cursor.fetchone()
+            if data:
+                cursor.execute("DELETE FROM cie_tracker WHERE chat_id = ?",(chat_id,))
+                conn.commit()
+                return True
+            else:
+                return False
+    except Exception as e:
+        print(f"Error deleting the cie_tracker details : {e}")
+
+async def get_all_cie_tracker_chat_ids():
+    """
+    This function is used to return all the chat_id that are present in the cie tracker table
+    :return: returns a tuple which contains all the chat_ids
+    """
+    try:
+        with sqlite3.connect(MANAGERS_DATABASE) as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT chat_id FROM cie_tracker")
+            tracker_chat_ids = [row[0] for row in cursor.fetchall()]
+            return tracker_chat_ids
+    except Exception as e:
+        print(f"Error retrieving all the chat_ids from cie_tracker : {e}")
+        return False
+
+async def get_cie_tracker_details(chat_id):
+    """
+    This function is used to get the tracker details from the database
+    :param chat_id: Chat id of the user
+    :return: returns a tuple
+    
+    tuple:
+    - status
+    - current_cie
+    """
+    try:
+        with sqlite3.connect(MANAGERS_DATABASE) as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT status,current_cie_marks FROM cie_tracker WHERE chat_id = ?",(chat_id,))
+            tracker_details = cursor.fetchone()
+            if tracker_details:
+                return tracker_details
+            else:
+                return None
+    except Exception as e:
+        print(f"Error retrieving the cie tracker details : {e}")
+        return False
