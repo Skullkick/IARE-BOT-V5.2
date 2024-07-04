b69911238ac2a9adfa45d511b61718142efbd9ee
Skullkick <121807883+Skullkick@users.noreply.github.com>
2024-07-04 18:28:23 +0530

    Enhanced User Login and Logout Functions
    Implemented user login and logout functionality for the "IARE BOT". Added functions for performing login with username and password, handling sessions, and providing logout options.

--- METHODS/operations.py
+++ METHODS/operations.py
@@ -83,3 +83,185 @@ async def is_user_logged_in(bot,message):
     chat_id = message.chat.id
     if await tdatabase.load_user_session(chat_id):
         return True
+async def perform_login( username, password):
+    """
+    Perform login with the provided username and password.
+
+    Returns:
+        bool: True if login is successful, False otherwise.
+    """
+    # Set up the necessary headers and cookies
+    cookies = {'PHPSESSID': ''}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
+        'Accept': '*/*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Origin': 'https://samvidha.iare.ac.in',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Referer': 'https://samvidha.iare.ac.in/index',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+    }
+
+    data = {
+        'username': username,
+        'password': password,
+    }
+
+    with requests.Session() as s:
+        index_url = "https://samvidha.iare.ac.in/index"
+        login_url = "https://samvidha.iare.ac.in/pages/login/checkUser.php"
+        home_url = "https://samvidha.iare.ac.in/home"
+
+        response = s.get(index_url)
+        cookie_to_extract = 'PHPSESSID'
+        cookie_value = response.cookies.get(cookie_to_extract)
+        cookies['PHPSESSID'] = cookie_value
+
+        s.post(login_url, cookies=cookies, headers=headers, data=data)
+
+        response = s.get(home_url)
+        if '<title>IARE - Dashboard - Student</title>' in response.text:
+
+            session_data = {
+                'cookies': s.cookies.get_dict(),
+                'headers': headers,
+                'username': username  # Save the username in the session data
+            }
+            return session_data
+        else:   
+            return None
+
+
+async def login(bot,message):
+    chat_id = message.chat.id
+    command_args = message.text.split()[1:]
+    # banned_usernames = await tdatabase.get_all_banned_usernames()
+    if not command_args:
+        username = ""
+    else:
+        username = command_args[0] # username of the user
+    if await tdatabase.get_bool_banned_username(username) is True: # Checks whether the username is in banned users or not.
+        return # Returns Nothing
+    if await tdatabase.load_user_session(chat_id): # Tries to get the cookies from the database.If found, it displays that you are already logged in.
+        await message.reply("You are already logged in.")
+        await buttons.start_user_buttons(bot,message)
+        await message.delete()
+        return
+
+    if len(command_args) != 2:
+        invalid_command_message =f"""
+```INVALID COMMAND USAGE
+⫸ How To Login:
+
+/login rollnumber password
+
+⫸ Example:
+
+/login 22951A0000 iare_unoffical_bot
+```
+        """
+        await message.reply(invalid_command_message)
+        return
+
+    password = command_args[1]
+    session_data = await perform_login(username, password)
+    # Initializes settings for the user
+    await user_settings.set_user_default_settings(chat_id)
+    if session_data:
+        await tdatabase.store_user_session(chat_id, json.dumps(session_data), username)
+        await tdatabase.store_username(username)
+        await message.delete()
+        await bot.send_message(chat_id,text="Login successful!")
+        if await pgdatabase.check_chat_id_in_pgb(chat_id) is False:
+
+            await message.reply_text("If you want to save your credentials Click on \"Yes\".",reply_markup =await buttons.start_save_credentials_buttons(username[:10],password))           
+        else:
+            await bot.send_message(chat_id,text="Your login information has already been registered.")
+        await buttons.start_user_buttons(bot,message)
+        
+    else:
+        await bot.send_message(chat_id,text="Invalid username or password.")
+
+async def auto_login_by_database(bot,message,chat_id):
+    # username,password = await pgdatabase.retrieve_credentials_from_database(chat_id) This Can be used if you want to take credentials from cloud database.
+    username,password = await tdatabase.fetch_credentials_from_database(chat_id) # This can be used to Fetch credentials from the Local database.
+    # Initializes settings for the user if the settings are not present
+    await user_settings.set_user_default_settings(chat_id)
+    if username != None:
+        username = username[:10]
+        if await tdatabase.get_bool_banned_username(username) is True: # Checks whether the username is in banned users or not.
+            # await tdatabase.delete_banned_username_credentials_data(username)
+            banned_username_chat_ids = await tdatabase.get_chat_ids_of_the_banned_username(username)
+            for chat_id in banned_username_chat_ids:
+                if await tdatabase.delete_user_credentials(chat_id) is True:
+                    if await pgdatabase.remove_saved_credentials_silent(chat_id) is True:
+                        return False
+        session_data = await perform_login(username, password)
+        if session_data:
+            await tdatabase.store_user_session(chat_id, json.dumps(session_data), username)  # Implement store_user_session function
+            await tdatabase.store_username(username)
+            await bot.send_message(chat_id,text="Login successful!")
+            return True
+        else:
+            if await tdatabase.check_chat_id_in_database(chat_id) is True:
+                await bot.send_message(chat_id,text="Unable to login using saved credentials, please try updating your password")
+            return False
+    else:
+        return False
+
+async def logout(bot,message):
+    chat_id = message.chat.id
+    session_data = await tdatabase.load_user_session(chat_id)
+    ui_mode = await user_settings.fetch_ui_bool(chat_id)
+    if ui_mode is None:
+        await user_settings.set_user_default_settings(chat_id)
+    if not session_data or 'cookies' not in session_data or 'headers' not in session_data:
+        if ui_mode[0] == 0:
+            await bot.send_message(chat_id,text=login_message_updated_ui)
+        elif ui_mode[0] == 1:
+            await bot.send_message(chat_id,text=login_message_traditional_ui)
+        return
+
+    logout_url = 'https://samvidha.iare.ac.in/logout'
+    session_data = await tdatabase.load_user_session(chat_id)
+    cookies,headers = session_data['cookies'], session_data['headers']
+    requests.get(logout_url, cookies=cookies, headers=headers)
+    await tdatabase.delete_user_session(chat_id)
+    await message.reply("Logout successful.")
+
+async def logout_user_and_remove(bot,message):
+    chat_id = message.chat.id
+    session_data = await tdatabase.load_user_session(chat_id)
+
+    if not session_data or 'cookies' not in session_data or 'headers' not in session_data:
+        await bot.send_message(chat_id,text="You are already logged out.")
+        return
+
+    logout_url = 'https://samvidha.iare.ac.in/logout'
+    session_data = await tdatabase.load_user_session(chat_id)
+    cookies,headers = session_data['cookies'], session_data['headers']
+    requests.get(logout_url, cookies=cookies, headers=headers)
+    await tdatabase.delete_user_session(chat_id)
+
+    await message.reply("Logout successful.")
+
+async def logout_user_if_logged_out(bot,chat_id):
+    session_data = await tdatabase.load_user_session(chat_id)
+    if not session_data or 'cookies' not in session_data or 'headers' not in session_data:
+        return
+
+    await tdatabase.delete_user_session(chat_id)
+
+    await bot.send_message(chat_id, text="Your session has been logged out due to inactivity.")
+
+async def silent_logout_user_if_logged_out(bot,chat_id):
+    session_data = await tdatabase.load_user_session(chat_id)
+    if not session_data or 'cookies' not in session_data or 'headers' not in session_data:
+        return
+
+    await tdatabase.delete_user_session(chat_id)
