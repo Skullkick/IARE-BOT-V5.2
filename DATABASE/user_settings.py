8d202a066d3f31dd343f010af92142cdbf749817
Skullkick <121807883+Skullkick@users.noreply.github.com>
2024-07-04 21:19:50 +0530

    Implemented creation of user settings and indexes tables

--- DATABASE/user_settings.py
+++ DATABASE/user_settings.py
@@ -12,3 +12,35 @@ async def create_user_settings_tables():
     """
     await create_user_settings_table()
     await create_indexes_table()
+
+async def create_user_settings_table():
+    """
+    Create the necessary tables for settings in the SQLite database.
+    """
+    with sqlite3.connect(SETTINGS_DATABASE) as conn:
+        cursor = conn.cursor()
+        # Create a table to store user sessions
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS user_settings (
+                chat_id INTEGER PRIMARY KEY,
+                attendance_threshold INTEGER DEFAULT 75,
+                biometric_threshold INTEGER DEFAULT 75,
+                traditional_ui BOOLEAN DEFAULT 0,
+                extract_title BOOLEAN DEFAULT 1  
+            )
+        """)
+        conn.commit()
+
+async def create_indexes_table():
+    """
+    Create the necessary table for the index values in sqlite database
+    """
+    with sqlite3.connect(SETTINGS_DATABASE) as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS index_values(
+                       name TEXT PRIMARY KEY,
+                       index_ TEXT
+            )
+        """)
+        conn.commit()
