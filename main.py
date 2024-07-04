0a1a050f8bb1cbc13ccc9f36d9d8fb82c86fabe5
Skullkick <121807883+Skullkick@users.noreply.github.com>
2024-07-04 18:09:41 +0530

    Start Command Handlers
    Implement command handlers for 'start', 'login', 'logout', 'report',  'help' and corresponding error handling.

--- main.py
+++ main.py
@@ -20,3 +20,45 @@ logging.basicConfig(level=logging.ERROR,
                         logging.FileHandler("bot_errors.log"),
                         logging.StreamHandler()
                     ])
+
+@bot.on_message(filters.command(commands=['start']))
+async def _start(bot,message):
+    try:
+        await operations.get_random_greeting(bot, message)
+    except Exception as e:
+        logging.error("Error in 'start' command: %s", e)
+@bot.on_message(filters.command(commands=['login']))
+async def _login(bot,message):
+    try:
+        await operations.login(bot, message)
+    except Exception as e:
+        logging.error("Error in 'login' command: %s", e)
+@bot.on_message(filters.command(commands=['logout']))
+async def _logout(bot,message):
+    try:
+        await operations.logout(bot, message)
+    except Exception as e:
+        logging.error("Error in 'logout' command: %s", e)
+
+@bot.on_message(filters.command(commands=['report']))
+async def _report(bot,message):
+    try:
+        await operations.report(bot, message)
+    except Exception as e:
+        logging.error("Error in 'report' command: %s", e)
+@bot.on_message(filters.command(commands=['help']))
+async def _help(bot,message):
+    try:
+        await operations.help_command(bot, message)
+    except Exception as e:
+        logging.error("Error in 'help' command: %s", e)
+@bot.on_message(filters.command(commands="settings"))
+async def settings_buttons(bot,message):
+    # Initializes settings for the user
+    chat_id = message.chat.id
+    try:
+        if await user_settings.fetch_user_settings(chat_id) is None:
+            await user_settings.set_user_default_settings(chat_id)
+        await buttons.start_user_settings(bot, message)
+    except Exception as e:
+        logging.error("Error in 'settings' command: %s", e)
