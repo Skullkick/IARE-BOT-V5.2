8881072d664038096f37ba4767e26f0d6cc3646e
Yuvaraj <yuvarajvasam03@gmail.com>
2024-07-04 23:46:11 +0530

    Imported all required modules.

---
+++ METHODS/lab_operations.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+import requests
+from DATABASE import user_settings,tdatabase
+from METHODS import operations,labs_handler,pdf_compressor
+from Buttons import buttons
+import os,re
