0aa85e930ed79d70fc0da8751701e1c00d58dc3a
varun <123943013+varun240s@users.noreply.github.com>
2024-07-04 22:02:34 +0530

    Added function to connect to PostgreSQL database
    Implemented `connect_pg_database` to establish a connection to PostgreSQL.

--- DATABASE/pgdatabase.py
+++ DATABASE/pgdatabase.py
@@ -1,1 +1,15 @@
+import asyncpg,os,json
 
+# Data stored in this database is permanent, only if user removes the data will be removed.
+
+async def connect_pg_database():
+    """connect_pg_database is used to make a connection to the postgres database"""
+    # connecting to the PSQL database
+    connection = await asyncpg.connect(
+        user=USER_CRED,
+        password=PASSWORD_CRED,
+        database=DATABASE_CRED,
+        host=HOST_CRED,
+        port=PORT_CRED
+    )
+    return connection
